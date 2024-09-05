dafb800bedab52d8d981c77dc0f12f250d5e9c0c
Stephan Anderson <10270741@uvu.edu>
2024-09-05 11:09:34 -0600

    Added fibonacci sequence

--- testing.py
+++ testing.py
@@ -1,1 +1,16 @@
 print('hello world')
+
+def Fibonacci(n):
+    if n < 0:
+        print("Invalid input")
+
+    elif n == 0:
+        return 0
+    
+    elif n == 1 or n == 2:
+        return 1
+    
+    else:
+        return Fibonacci(n-1) + Fibonacci(n-2)
+
+print(Fibonacci(9))
